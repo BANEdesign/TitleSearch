d32a8e3250aa3d857bad2098f104fcb10e67313d
Bryonna Baines <bbaines09@gmail.com>
2018-09-25 14:52:01 -0700

    configured package file for heroku deploy

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import ListsContainer from './components/ListsContainer';
 
+const http = require('http');
+const port=process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello World</h1>');
+});
+server.listen(port,() => {
+console.log(`Server running at port `+port);
+});
+
 class App extends Component {
   render() {
     return (
